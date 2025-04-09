043c5f5f4273263280f3080bb28a7ce05729472c
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2025-04-09 22:04:52 +0530

    Create Hello.cs
    Initial Code

---
+++ Greeting Project/Hello.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Hello, "+textBox1.Text;
+        }
+    }
+}
